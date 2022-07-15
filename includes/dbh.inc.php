16824de760346d10ed2fd9840321ad152097b057
Bradley Wayne Arnold <82877822+B1r2a3d4l5y@users.noreply.github.com>
2022-07-15 11:32:41 +0200

    Delete dbh.inc.php

--- includes/dbh.inc.php
+++
@@ -1,8 +0,0 @@
-<?php
-$dbServerName = "localhost";
-$dbUserName = "root";
-$dbPassword = "";
-$dbName = "expense_user_sign_up";
-
-
-$conn = mysqli_connect($dbServerName ,$dbUserName, $dbPassword,$dbName  );
